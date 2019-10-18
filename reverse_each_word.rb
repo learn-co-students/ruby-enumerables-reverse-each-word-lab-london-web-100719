baea20f1e5a69e0e423d688c7edda4a3219a9ad2
Qing <chingwang.qw@gmail.com>
2019-10-18 12:50:45 +0100

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,15 +3,14 @@ def reverse_each_word(sentence1)
 	result_array = []
   array.each do |sentence1|
     result_array << sentence1.reverse 
-end 
- result_array.join(" ")
- end 
-
-def reverse_each_word(sentence2) 
-  array = sentence2.split(" ")
-  result_array = []
-  array.collect do |sentence2|
-    result_array << sentence2.reverse
   end 
-  result_array.join(" ")
-end 
+ result_array.join(" ")
+ end  
+
+def reverse_each_word(sentence2)
+  split_words = sentence2.split(" ")
+  reverse_words = split_words.collect do |w| 
+    w.reverse
+  end
+  reverse_words.join(" ")
+end
